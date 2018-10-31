0d134740bee8152caa0f87ced92a6c0857227586
swsgit <sws@tuta.io>
2018-10-31 07:54:08 -0700

    Delete run.bat

--- src/run.bat
+++
@@ -1,8 +0,0 @@
-@ECHO OFF
-set debug=-Wall -Wextra -g
-set std=-std=gnu11
-set include=-fopenmp
-set op=-Ofast
-set asm=gcc prime18.c %std% %include% %op% -fverbose-asm -S -o prime18_op.S
-%asm%
-gcc prime18.c %debug% %std% %include% %op% -o prime18
